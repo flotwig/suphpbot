0ec0602f776b138a0e176692b8f5c3682c065cef
StompingBrokenGlass <StompingBrokenGlass@gmail.com>
2013-03-21 02:03:38 +0300

    Added Licence information

---
+++ modules/metal_archives.php
@@ -0,0 +1,16 @@
+<?php
+//** Author: StompingBrokenGlass (StompingBrokebGlass@Gmail.com) **//
+/*
+//   Description: Metal-Archives.com API Implementation in Suphpbot,
+//   Based on The Java API for the Encyclopedia Metallum, the work of
+//   Phillip Wirth (loooki@gulli.com).
+//
+//   Licence: Public Domain, with Beer-ware revision 42 extention.
+//
+//   If the user and Phillip Wirth ever met, and thought that this
+//   plugin based on his work is worth it, then the user can buy 
+//   a beer to Phillip Wirth as thanks for his work.
+//
+*/
+
+?>
