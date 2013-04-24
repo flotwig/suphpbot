6da9ccfc9f2e370ecc9cf85a3f01f700e937ca64
StompingBrokenGlass <StompingBrokenGlass@gmail.com>
2013-04-24 10:55:00 +0300

    reterving data from Metal-Archives

--- modules/metal_archives.php
+++ modules/metal_archives.php
@@ -3,7 +3,8 @@
 /*
 //   Description: Metal-Archives.com API Implementation in Suphpbot,
 //   Based on The Java API for the Encyclopedia Metallum, the work of
-//   Phillip Wirth (loooki@gulli.com).
+//   Phillip Wirth (loooki@gulli.com), and last.fm plugin by 
+//   Kwamaking (kwamaking@gmail.com).
 //
 //   Licence: Public Domain, with Beer-ware revision 42 extention.
 //
@@ -13,28 +14,90 @@
 //
 */
 
-// plugin required varibles
+// Plugin required varibles
 $scriptname = str_replace(".php","",basename(__FILE__));
 
 $function_map[$scriptname]= array(
-     'm-band' => 'ma_band'
+     'ma-band' => 'ma_band'
 );
 
 $help_map[$scriptname] = array (
-     'm-band' => 'Search Metal-Archives for band'
+     'ma-band' => 'Search Metal-Archives for band'
 );
 
-// Predefined Variables
-$MA_BASE_URL = "http://www.metal-archives.com/";
+// Functions
+
+function ma_getData($queryURL) {
+	global $loaded_modules, $channel,$MA_BASE_URL ;
+
+	$MA_BASE_URL = "http://www.metal-archives.com/";
+
+	if (in_array("internets", $loaded_modules)) {
+		$message = $MA_BASE_URL . $queryURL;
+
+		$message = @internets_get_contents($MA_BASE_URL . $queryURL);
+
+		$message = json_decode($message, TRUE);
+	} else {
+		send_msg($channel, "This will not work without the internets module.");
+	}
+
+	return $message;
+}
 
 function ma_band () {
-     global $args,$channel,$nick;
 
-     $band = $args[0];
+     /**************************************************************
+     ************************* Sample Data *************************
 
-     $str = "searching " . $band;
-     
+     * Sample query URL
+	http://www.metal-archives.com/search/ajax-advanced/searching/bands/?bandName=test&exactBandMatch=0&sEcho=1&iColumns=3&sColumns=&iDisplayStart=0&iDisplayLength=20&sNames=%2C%2C
+
+     * Sample response
+	{ 
+		"error": "",
+		"iTotalRecords": 11,
+		"iTotalDisplayRecords": 11,
+		"sEcho": 0,
+		"aaData": [
+					[ 
+				"<a href=\"http://www.metal-archives.com/bands/Test/3540355114\">Test</a>  <!-- 8.284618 -->" ,	
+				"Grindcore/Death Metal/Crustcore" ,
+				"Brazil"    		]
+					,
+							[ 
+				"<a href=\"http://www.metal-archives.com/bands/De-Test/22778\">De/Test</a> (<strong>a.k.a.</strong> DeTest) <!-- 3.9795065 -->" ,	
+				"Thrash Metal" ,
+				"Germany"    		]
+					]
+	}
+
+     ************************* End Sample Data *************************
+     ******************************************************************/
+
+     global $args,$channel,$nick,$arguments;
+
+     $band = $arguments;
+
+     $str = "";
+
+     // Empty check
+     if ((is_null($band)) || ($band == "")) {
+
+          $str ="Kindly provide the band name";
+
+     } else {
+          $str = "searching " . $band;
+          $queryUrl = "search/ajax-advanced/searching/bands/?bandName=" .
+          urlencode($band) . "&exactBandMatch=0&sEcho=1&iColumns=3&sColumns=&iDisplayStart=0&iDisplayLength=20&sNames=%2C%2C";
+
+          $response = ma_getData($queryUrl);
+
+          $str = $response["iTotalRecords"];
+     }
+
      send_msg($channel,$str);
+
 }
 
 ?>
